60d2d09612b9c1c03b6d0938aa188f6223913fea
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-16 16:54:35 +0100

    First commit

--- app/src/main/java/irawan/electroshock/sevicedemo/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/sevicedemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package irawan.electroshock.sevicedemo
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import irawan.electroshock.sevicedemo.databinding.ActivityMainBinding
@@ -8,22 +11,58 @@ import irawan.electroshock.sevicedemo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var isStarted: Boolean = false
+    private lateinit var serviceIntent:Intent
+    private var time = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val serviceIntent = Intent(this, MyBackgroundServices::class.java)
-        serviceIntent.putExtra("Data", "Data1")
-        serviceIntent.putExtra("Data1", 100)
-
         binding.btnStart.setOnClickListener {
-            startService(serviceIntent)
+            startOrStop()
         }
 
-        binding.btnStop.setOnClickListener {
-            stopService(serviceIntent)
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+
+        serviceIntent = Intent(applicationContext, StopWatchServices::class.java)
+        registerReceiver(updateTime, IntentFilter(StopWatchServices.UPDATED_TIME))
+    }
+
+    private fun startOrStop(){
+        if (isStarted) {
+            stop()
+        } else {
+            start()
+        }
+    }
+
+    private fun start(){
+        serviceIntent.putExtra(StopWatchServices.CURRENT_TIME, time)
+        startService(serviceIntent)
+        binding.btnStart.text = "Stop"
+        isStarted = true
+
+    }
+
+    private fun stop(){
+        stopService(serviceIntent)
+        binding.btnStart.text = "Start"
+        isStarted = false
+
+    }
+
+    private fun reset(){
+
+    }
+
+    private val updateTime : BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchServices.CURRENT_TIME, 0.0)
+            binding.tvTime.text = time.toString()
         }
     }
 }
---
+++ app/src/main/java/irawan/electroshock/sevicedemo/StopWatchServices.kt
@@ -0,0 +1,47 @@
+package irawan.electroshock.sevicedemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import java.util.*
+
+class StopWatchServices : Service() {
+
+    private val timer = Timer()
+
+    init{
+        Log.i("MyTag", "Service has been created")
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(CURRENT_TIME,0.0)
+        timer.scheduleAtFixedRate(StopWatchTimerTask(time),0,100)
+
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+
+    companion object{
+        const val CURRENT_TIME = "current time"
+        const val UPDATED_TIME = "updated time"
+    }
+
+    private inner class StopWatchTimerTask(private var time:Double):TimerTask(){
+        override fun run() {
+            val intent = Intent(UPDATED_TIME)
+            time++
+            intent.putExtra(CURRENT_TIME, time)
+            sendBroadcast(intent)
+        }
+
+    }
+}
